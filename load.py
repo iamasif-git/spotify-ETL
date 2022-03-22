9b478245a420a014d6e60be57cecd6a36b66353d
Md Asif <mdasif.uem#gmail.com>
2022-03-23 03:40:07 +0530

    added try except block

--- load.py
+++ load.py
@@ -7,16 +7,26 @@ import sqlalchemy
 df = transform.send_to_load()
 
 engine = sqlalchemy.create_engine(DATABASE_CONNECTION)
-conn = sqlite3.connect("mysongs.sqlite")
+conn = sqlite3.connect("mysongs.db")
 cursor = conn.cursor()
 
 query = """
-CREATE TABLE IF NOT EXISTS my_played_tracks (
+CREATE TABLE IF NOT EXISTS my_played_tracks(
 song_name VARCHAR(100),
-artist VARCHAR(100),
-played_at(VARCHAR(100),
-timestamp VARCHAR(100),
-CONSTRAINT primary_key_constraint PRIMARY KEY (played_at) 
+artist_name VARCHAR(100),
+played_at VARCHAR(100) PRIMARY KEY,
+timestamp VARCHAR(100)
 );
 """
+
 cursor.execute(query)
+print("Opened Database connection")
+
+
+try:
+    df.to_sql("my_played_tracks",engine,index=False,if_exists='append')
+except:
+    print("data already exists in database")    
+    
+conn.close()
+print("Connection closed")    
