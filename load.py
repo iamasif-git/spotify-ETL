c7d50541b880915010c7edf0107fb443a65a23e2
Md Asif <mdasif.uem#gmail.com>
2022-03-23 03:38:49 +0530

    added database connection

--- load.py
+++ load.py
@@ -1,4 +1,22 @@
+import sqlite3
+from extract import DATABASE_CONNECTION
 import transform
+import sqlalchemy
+
 
 df = transform.send_to_load()
-print(df)
+
+engine = sqlalchemy.create_engine(DATABASE_CONNECTION)
+conn = sqlite3.connect("mysongs.sqlite")
+cursor = conn.cursor()
+
+query = """
+CREATE TABLE IF NOT EXISTS my_played_tracks (
+song_name VARCHAR(100),
+artist VARCHAR(100),
+played_at(VARCHAR(100),
+timestamp VARCHAR(100),
+CONSTRAINT primary_key_constraint PRIMARY KEY (played_at) 
+);
+"""
+cursor.execute(query)
